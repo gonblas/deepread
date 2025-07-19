e058508c9e98b33a4cd06eac90ef78141e16240b
Gonzalo Blasco <gonzagabino@gmail.com>
2025-07-19 18:35:00 -0300

    feat: enhance UserRepositoryTest with setup and teardown methods for user persistence tests

--- backend/src/test/java/gblas/books/backend/UserRepositoryTest.java
+++ backend/src/test/java/gblas/books/backend/UserRepositoryTest.java
@@ -1,4 +1,42 @@
 package gblas.books.backend;
 
+import gblas.books.backend.entity.UserEntity;
+import gblas.books.backend.repository.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
 public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private UserEntity testUser;
+
+    @BeforeEach
+    public void setup() {
+        userRepository.deleteAll();
+        testUser = new UserEntity();
+        testUser.setEmail("example@xyz.com");
+        testUser.setUsername("example");
+        testUser.setHashed_password("this_is_an_example");
+        userRepository.save(testUser);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        userRepository.delete(testUser);
+    }
+
+    @Test
+    void givenUser_whenSaved_thenCanBeFoundById() {
+        UserEntity savedUser = userRepository.findByEmail(testUser.getEmail()).orElse(null);
+        assertNotNull(savedUser);
+    }
 }
